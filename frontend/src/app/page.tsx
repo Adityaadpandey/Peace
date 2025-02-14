2a9870c0565e0698c2e93cbbc8b5578fe1724a6e
Aditya Pandey <adityapandeyadp@gmail.com>
2025-02-14 23:50:05 +0530

    good to fo

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,31 @@
+'use client';
+import Spline from '@splinetool/react-spline';
+import { Application } from '@splinetool/runtime';
+import { Orbitron } from 'next/font/google';
+import Navbar from './_components/Navbar';
 
-const page = () => {
+const orbitron = Orbitron({ subsets: ['latin'], weight: ['700'] });
+
+export default function Home() {
+  function onLoad(spline: Application) {
+    // spline.play();
+    spline.canvas.style.scale = '1.5';
+  }
+
   return (
-    <div>page</div>
-  )
+    <div>
+      <main className='max-h-screen overflow-hidden'>
+        <Navbar className='fixed -z-10' />
+        <Spline
+          onLoad={onLoad}
+          scene="https://prod.spline.design/GGFqhs3CbugFAs4K/scene.splinecode"
+        />
+      </main>
+      <footer className='fixed bottom-0 h-[20dvh] w-screen flex justify-between -z-10'>
+        <div className={`${orbitron.className} text-8xl p-4 text-center flex-1`}>
+          Sukoon AI
+        </div>
+      </footer>
+    </div>
+  );
 }
-
-export default page
